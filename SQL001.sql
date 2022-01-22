0aa89bddf60b42033a32cf1e2921a5709eca6173
shashisah1001 <shashisah1001@gmail.com>
2022-01-22 23:38:55 +0530

    sql command trials

---
+++ SQL001.sql
@@ -0,0 +1,14 @@
+select * from detail
+select top 3 package from detail order by PACKAGE desc;
+
+insert into detail(sno,name,mobile,address,college,PACKAGE)values(11,'shashi',7766000298,'bagodar','techno',200000);
+
+delete from detail where sno=1;
+
+select * from work;
+
+
+select max(package) from detail
+
+
+
